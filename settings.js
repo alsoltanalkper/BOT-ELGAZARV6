0ad03e37fb6aa4ba5786335fad03c50770dcb459
alsoltanalkper <134983927+alsoltanalkper@users.noreply.github.com>
2023-06-22 05:31:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,9 +15,9 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
+global.owner = ['201140627942'] //ur owner number
+global.ownername = "aALSOLTAN  //ur owner name
+global.ytname = "YT: alsoltan" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
